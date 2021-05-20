8a00c00cf86b235d248a3e9ee9e58a8438d98647
Noman5237 <anonyman637@gmail.com>
2021-05-21 02:15:24 +0600

    Deprecating direct use of Item class and updating items on day end listener update

--- src/main/java/inventory/Inventory.java
+++ src/main/java/inventory/Inventory.java
@@ -1,62 +1,28 @@
 package inventory;
 
-class Inventory {
-	Item[] items;
+import java.util.Arrays;
+
+public class Inventory {
+	
+	public Item[] items;
 	
 	public Inventory(Item[] items) {
 		this.items = items;
 	}
 	
-	public void updateQuality() {
-		for (int i = 0; i < items.length; i++) {
-			if (!items[i].name.equals("Aged Brie")
-					&& !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-				if (items[i].quality > 0) {
-					if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-						items[i].quality = items[i].quality - 1;
-					}
-				}
-			} else {
-				if (items[i].quality < 50) {
-					items[i].quality = items[i].quality + 1;
-					
-					if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-						if (items[i].sellIn < 11) {
-							if (items[i].quality < 50) {
-								items[i].quality = items[i].quality + 1;
-							}
-						}
-						
-						if (items[i].sellIn < 6) {
-							if (items[i].quality < 50) {
-								items[i].quality = items[i].quality + 1;
-							}
-						}
-					}
-				}
-			}
-			
-			if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-				items[i].sellIn = items[i].sellIn - 1;
-			}
-			
-			if (items[i].sellIn < 0) {
-				if (!items[i].name.equals("Aged Brie")) {
-					if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-						if (items[i].quality > 0) {
-							if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-								items[i].quality = items[i].quality - 1;
-							}
-						}
-					} else {
-						items[i].quality = items[i].quality - items[i].quality;
-					}
-				} else {
-					if (items[i].quality < 50) {
-						items[i].quality = items[i].quality + 1;
-					}
-				}
+	public void processDayEnd() {
+		for (Item item : items) {
+			try {
+				ImprovedItem improvedItem = (ImprovedItem) item;
+				improvedItem.processDayEnd();
+			} catch (ClassCastException e) {
+				throw new UnsupportedOperationException("Cannot update objects of deprecated Item class. Please consider using ImprovedItem");
 			}
 		}
+	}
+	
+	@Override
+	public String toString() {
+		return Arrays.toString(items);
 	}
 }
