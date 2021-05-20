a5d617c1f096614c2f177772dccef41ec5256e28
Noman5237 <anonyman637@gmail.com>
2021-05-21 02:13:32 +0600

    Extended Item class to provide additional features using functional compositions

---
+++ src/main/java/inventory/ImprovedItem.java
@@ -0,0 +1,28 @@
+package inventory;
+
+public class ImprovedItem extends Item {
+	
+	public interface Updater {
+		
+		void onDayEnd(ImprovedItem item);
+	}
+	
+	private Updater updater;
+	
+	private ImprovedItem(String name, int sellIn, int quality) {
+		super(name, sellIn, quality);
+	}
+	
+	public static ImprovedItem CreateImprovedItem(String name, int sellIn, int quality) {
+		return new ImprovedItem(name, sellIn, quality);
+	}
+	
+	public void setQualityUpdater(Updater updater) {
+		this.updater = updater;
+	}
+	
+	public void processDayEnd() {
+		this.updater.onDayEnd(this);
+	}
+	
+}
