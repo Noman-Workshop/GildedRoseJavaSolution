67637e7256236f508b7184e1d3370900e6f10c7a
Noman5237 <anonyman637@gmail.com>
2021-05-23 01:27:02 +0600

    Core API test and deprecation passed

--- src/test/java/inventory/InventoryTest.java
+++ src/test/java/inventory/InventoryTest.java
@@ -4,7 +4,9 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 
+import static inventory.ImprovedItem.CreateItem;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class InventoryTest {
 	
@@ -12,7 +14,7 @@ class InventoryTest {
 	
 	@Test
 	void InventoryCoreAPITest() {
-		Item[] items = new Item[] {new Item("foo", 0, 0)};
+		Item[] items = new Item[] {CreateItem("foo", 0, 0)};
 		System.out.println("Items created: " + Arrays.toString(items));
 		app = new Inventory(items);
 		System.out.println("Inventory created: " + app);
@@ -21,6 +23,19 @@ class InventoryTest {
 		assertEquals(-1, getTestItem().sellIn);
 		assertEquals(0, getTestItem().quality);
 		System.out.println("After 1 call to update quality inventory status: " + app);
+	}
+	
+	@Test
+	void DeprecatedItemUseTest() {
+		Item[] items = new Item[] {new Item("foo", 0, 0)};
+		System.out.println("Items created: " + Arrays.toString(items));
+		app = new Inventory(items);
+		System.out.println("Inventory created: " + app);
+		assertThrows(Exception.class, () -> app.processDayEnd());
+		assertEquals(0, getTestItem().sellIn);
+		assertEquals(0, getTestItem().quality);
+		System.out.println("After 1 failed call to update quality inventory status: " + app);
+		System.out.println("update using of direct use of Item class is now deprecated forcefully.");
 	}
 	
 	/* ================================ UTILITY ==================================== */
